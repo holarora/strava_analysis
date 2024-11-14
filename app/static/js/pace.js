1a4abe07dbd7f4e620e100ff62fda91fc109f508
holarora <liu.lijia.82v@st.kyoto-u.ac.jp>
2024-11-14 10:28:35 +0900

    pace graph

---
+++ app/static/js/pace.js
@@ -0,0 +1,52 @@
+fetch('/data')
+    .then(response => response.json())
+    .then(chartData => {
+        const ctx = document.getElementById('myChart').getContext('2d');
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: chartData.labels,
+                datasets: chartData.datasets.map(dataset => ({
+                    ...dataset,
+                    borderWidth: 2,
+                    pointRadius: 4,
+                    pointHoverRadius: 6,
+                }))
+            },
+            options: {
+                responsive: true,
+                interaction: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                stacked: false,
+                scales: {
+                    x: {
+                        type: 'category',
+                        display: true,
+                        position: 'left',
+                        title: {
+                            display: true,
+                            text: 'Week Start'
+                        },
+                    },
+                    y: {
+                        type: 'linear',
+                        display: true,
+                        position: 'left',
+                        title: {
+                            display: true,
+                            text: 'Pace (min/km)'
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Pace graph'
+                    }
+                }
+            }
+        });
+    })
+    .catch(error => console.error('Error fetching chart data:', error));
